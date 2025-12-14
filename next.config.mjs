e3f8f3bbc710d6f6d4f887d834f01b89e2f66de0
Samuel Olamide <sam99kupo@gmail.com>
2025-12-14 14:16:07 +0100

    fix prod deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,11 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production' || process.env.GITHUB_ACTIONS === 'true';
-const basePath = isProd ? '/portfolio' : '';
-
 const nextConfig = {
   output: 'export',
-  basePath: basePath,
-  assetPrefix: isProd ? '/portfolio/' : '',
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
+  basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio/' : '',
 };
 
 export default nextConfig;
